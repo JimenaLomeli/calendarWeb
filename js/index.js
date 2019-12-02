494e1f91686a38d92c8fe1a07235050267da43e9
Ana Paola Treviño <anapao.trevino@gmail.com>
2019-12-02 15:13:12 -0600

    Validations

--- js/index.js
+++ js/index.js
@@ -1,31 +1,59 @@
 $('#login_button').on('click', function(){
-  // cargar email y password de su html
+  let valEmail
+  let valPassword 
+  
   let email = $('#email').val()
   let password = $('#password').val()
 
-  json_to_send = {
-    "email": email,
-    "password" : password
-  };
+  let email_error = $('#email_error')
+  let password_error = $('#password_error')
 
-  json_to_send = JSON.stringify(json_to_send)
-  console.log(json_to_send)
-  $.ajax({
-    url: 'https://calendario-back.herokuapp.com/login',
-    headers: {
-        'Content-Type':'application/json'
-    },
-    method: 'POST',
-    dataType: 'json',
-    data: json_to_send,
-    success: function(data){
-      // guardar token en localstorage o cookie
-      localStorage.setItem('token', data.token)
-      window.location = './calendar.html'
-    },
-    error: function(error_msg) {
-      console.log(error_msg)
-      // alert((error_msg["responseText"]))
-    }
-  })
+  if (email === "" || !valida_email(email.value)) {
+    email_error.removeClass('hidden');
+    valEmail = false
+  } else {
+    email_error.addClass('hidden');
+    valEmail = true
+  }
+
+  if (password === "") {
+    password_error.removeClass('hidden');
+    valPassword = false
+  }
+  else {
+    password_error.addClass('hidden');
+    valPassword = true
+  }
+
+  if (valPassword && valEmail) {
+    json_to_send = {
+      "email": email,
+      "password" : password
+    };
+    json_to_send = JSON.stringify(json_to_send)
+  
+    $.ajax({
+      url: 'https://calendario-back.herokuapp.com/login',
+      headers: {
+          'Content-Type':'application/json'
+      },
+      method: 'POST',
+      dataType: 'json',
+      data: json_to_send,
+      success: function(data){
+        // guardar token en localstorage o cookie
+        localStorage.setItem('token', data.token)
+        window.location = './calendar.html'
+      },
+      error: function(error_msg) {
+        console.log(error_msg)
+        // alert((error_msg["responseText"]))
+      }
+    })
+  }
 })
+
+function valida_email(email) {
+    var regexp_email = /\S+@\S+\.\S+/;
+    return regexp_email.test(email);
+}
--- js/signup.js
+++ js/signup.js
@@ -1,32 +1,72 @@
 $('#signup_button').on('click', function(){
-  // cargar los valores de password, email, name, age
+  let valEmail
+  let valPassword 
+  let valName
+
   let password = $("#password").val()
   let email = $("#email").val()
   let name = $("#name").val()
 
-  json_to_send = {
-    "password" : password,
-    "email": email,
-    "name": name,
-  };
+  let email_error = $('#email_error')
+  let password_error = $('#password_error')
+  let name_error = $("#name_error")
 
-  json_to_send = JSON.stringify(json_to_send);
+  if (email === "" && !valida_email(email.value)) {
+    email_error.removeClass('hidden');
+    valEmail = false
+  } else {
+    email_error.addClass('hidden');
+    valEmail = true
+  }
 
-  $.ajax({
-    url: 'https://calendario-back.herokuapp.com/users',
-    headers: {
-        'Content-Type':'application/json'
-    },
-    method: 'POST',
-    dataType: 'json',
-    data: json_to_send,
-    success: function(data){
-      alert("Usuario creado con exito");
-      console.log('success: '+ data);
-      window.location = './index.html'
-    },
-    error: function(error_msg) {
-      alert((error_msg['responseText']));
-    }
-  });
+  if (password === "") {
+    password_error.removeClass('hidden');
+    valPassword = false
+  }
+  else {
+    password_error.addClass('hidden');
+    valPassword = true
+  }
+
+  if (name === "") {
+    name_error.removeClass('hidden');
+    valName = false
+  }
+  else {
+    name_error.addClass('hidden');
+    valName = true
+  }
+
+  if (valPassword && valEmail && valName) {
+    json_to_send = {
+      "password" : password,
+      "email": email,
+      "name": name,
+    };
+
+    json_to_send = JSON.stringify(json_to_send);
+
+    $.ajax({
+      url: 'https://calendario-back.herokuapp.com/users',
+      headers: {
+          'Content-Type':'application/json'
+      },
+      method: 'POST',
+      dataType: 'json',
+      data: json_to_send,
+      success: function(data){
+        alert("Usuario creado con exito");
+        console.log('success: '+ data);
+        window.location = './index.html'
+      },
+      error: function(error_msg) {
+        alert((error_msg['responseText']));
+      }
+    });
+  }
 });
+
+function valida_email(email) {
+  var regexp_email = /\S+@\S+\.\S+/;
+  return regexp_email.test(email);
+}
